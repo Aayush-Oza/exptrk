84f7fb79c63eff8579133512e6c3d6878d1e426c
Aayush Oza <aayushoza2804@gmail.com>
2026-01-11 15:51:53 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,17 +3,14 @@ from extensions import db
 
 class User(db.Model):
     __tablename__ = "users"
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-
 class Transaction(db.Model):
     __tablename__ = "transactions"
-
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     amount = db.Column(db.Numeric(10, 2), nullable=False)
@@ -25,10 +22,8 @@ class Transaction(db.Model):
     date = db.Column(db.Date)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-
 class PeopleLedger(db.Model):
     __tablename__ = "people_ledger"
-
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     person_name = db.Column(db.String(100))
